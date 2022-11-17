c806c952e7a0357771265ff65feee2acc6b92e41
e759ksh <e759ksh@naver.com>
2022-11-17 18:42:41 +0900

    server

--- frontend/src/api/post.js
+++ frontend/src/api/post.js
@@ -4,7 +4,7 @@ import { setInterceptors } from "@/api/interceptor";
 
 function crateInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: "http://3.37.227.155/",
   });
   return setInterceptors(instance);
 }

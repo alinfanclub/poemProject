4e1c00e773a3a725581441828929dcf5ebb8a3fa
e759ksh <e759ksh@naver.com>
2022-11-22 16:30:24 +0900

    cors fix

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -4,7 +4,7 @@ import { setInterceptors } from "@/api/interceptor";
 
 function crateInstance() {
   const instance = axios.create({
-    baseURL: "http://13.231.63.76:3000/user",
+    baseURL: "https://d1cuv13t5kqbgp.cloudfront.net/user",
   });
   return setInterceptors(instance);
 }

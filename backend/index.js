94421ea994686d9c40f7275625c9c7849d3dc1e7
e759ksh <e759ksh@naver.com>
2022-11-24 12:05:12 +0900

    cors 옵션 설정

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,11 @@ const sever = async () => {
         origin: "*",
       })
     );
-
+    app.all("/*", function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
     // api
     app.use("/user", router);
     app.use("/post", Postrouter);
--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -4,7 +4,7 @@ import { setInterceptors } from "@/api/interceptor";
 
 function crateInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:3000/user",
+    baseURL: "https://kodpp.com/user",
   });
   return setInterceptors(instance);
 }

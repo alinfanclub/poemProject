348ad01c95c1d90372f84c840f44b0b9eb754fcf
e759ksh <e759ksh@naver.com>
2022-11-24 12:39:25 +0900

    cors 옵션 설정

--- backend/index.js
+++ backend/index.js
@@ -19,16 +19,17 @@ const sever = async () => {
     mongoose.set("debug", true);
     console.log("mongoDB is connected!");
     app.use(express.json());
+    app.all("/*", function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "https://kodpp.com");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PUT,DELETE");
+      next();
+    });
     app.use(
       cors({
         origin: "*",
       })
     );
-    app.all("/*", function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "https://kodpp.com");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      next();
-    });
     // api
     app.use("/user", router);
     app.use("/post", Postrouter);
--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -4,7 +4,7 @@ import { setInterceptors } from "@/api/interceptor";
 
 function crateInstance() {
   const instance = axios.create({
-    baseURL: "https://kodpp.com/user",
+    baseURL: "13.231.63.76:3000/user",
   });
   return setInterceptors(instance);
 }

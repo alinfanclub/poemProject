6ff3b7f30a398ff3a5f8e8e799a6991fda22dcea
e759ksh <e759ksh@naver.com>
2022-11-24 13:08:11 +0900

    cors 옵션 설정

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const sever = async () => {
     mongoose.set("debug", true);
     console.log("mongoDB is connected!");
     app.use(express.json());
-    app.all("/*", function (req, res, next) {
+    app.use("/*", function (req, res, next) {
       res.header("Access-Control-Allow-Origin", "https://kodpp.com");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
       res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PUT,DELETE");

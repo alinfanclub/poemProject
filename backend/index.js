ece6bb1fdd97dfffd9bce45821f83408fd517af4
e759ksh <e759ksh@naver.com>
2022-11-24 12:09:33 +0900

    cors 옵션 설정

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const sever = async () => {
       })
     );
     app.all("/*", function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Origin", "https://kodpp.com");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
       next();
     });

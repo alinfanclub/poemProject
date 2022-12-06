949a1e33339eb8239fabf229c91ed669226f7a73
e759ksh <e759ksh@naver.com>
2022-12-06 19:24:48 +0900

    comment 삭제 기능 추가

--- frontend/src/api/comment.js
+++ frontend/src/api/comment.js
@@ -2,20 +2,20 @@ import axios from "axios";
 // import store from "@/store/index";
 import { setInterceptors } from "@/api/interceptor";
 
-function crateInstance() {
-  const instance = axios.create({
-    baseURL: "http://localhost:3000/comment",
-  });
-  return setInterceptors(instance);
-}
-
 // function crateInstance() {
 //   const instance = axios.create({
-//     baseURL:
-//       "https://port-0-poemproject-11er1a24lbbi8bv8.gksl2.cloudtype.app/comment",
+//     baseURL: "http://localhost:3000/comment",
 //   });
 //   return setInterceptors(instance);
 // }
+
+function crateInstance() {
+  const instance = axios.create({
+    baseURL:
+      "https://port-0-poemproject-11er1a24lbbi8bv8.gksl2.cloudtype.app/comment",
+  });
+  return setInterceptors(instance);
+}
 
 const instance = crateInstance();
 

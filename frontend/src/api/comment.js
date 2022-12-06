c2f6f62669aee88a2d7043a4eeadcb5678f3ab35
e759ksh <e759ksh@naver.com>
2022-12-06 12:41:26 +0900

    임시저장 버튼 삭제 및 댓글 기능 활성화(CR)

--- frontend/src/api/comment.js
+++ frontend/src/api/comment.js
@@ -2,9 +2,16 @@ import axios from "axios";
 // import store from "@/store/index";
 import { setInterceptors } from "@/api/interceptor";
 
+// function crateInstance() {
+//   const instance = axios.create({
+//     baseURL: "http://localhost:3000/comment",
+//   });
+//   return setInterceptors(instance);
+// }
 function crateInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:3000/comment",
+    baseURL:
+      "https://port-0-poemproject-11er1a24lbbi8bv8.gksl2.cloudtype.app/comment",
   });
   return setInterceptors(instance);
 }
